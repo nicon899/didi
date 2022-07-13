a723906058f7e1664c14de02a62d17639351e383
Nico Neubig <nico@neubig.name>
2022-07-13 21:11:55 +0200

    Header

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li class="{$page.url.pathname === '/' ? 'activeTab' : ''}"><a sveltekit:prefetch href="/">Home</a></li>
+			<li class="{$page.url.pathname === '/dives' ? 'activeTab' : ''}">
+				<a sveltekit:prefetch href="/dives">Sprünge</a>
+			</li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+
+	<div class="corner">
+		<!-- TODO put something else here? github link? -->
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		background-color: orangered;
+		height: 50px;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li.activeTab {
+		background-color: #FF222280;
+		color: white;
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1em;
+		color: var(--heading-color);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--accent-color);
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-width: 1024px;
+		min-height: calc(100vh - 75px);
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background-color: orangered;
+		height: 25px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+</style>
